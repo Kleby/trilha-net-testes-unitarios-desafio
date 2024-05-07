1e4cbc17acf56327d20e7a90c5878d47db784faf
Kleby <kleby10@gmail.com>
2024-05-07 11:19:26 -0300

    Desafio concluido

--- TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
@@ -4,7 +4,7 @@ namespace TestesUnitarios.Desafio.Console.Services
     {
         public int RetornarQuantidadeCaracteres(string texto)
         {
-            var numeroCaracteres = texto.Length;
+            int numeroCaracteres = texto.Length;
             return numeroCaracteres;
         }
 
